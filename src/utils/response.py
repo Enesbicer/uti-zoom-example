286a7fefcc48bc9d358ff209da1af502551faea3
Enesbicer <enes.b4154@gmail.com>
2025-07-14 15:52:45 +0300

    feat: response.py Update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,26 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
 from components.ZoomExample.src.models.PackageModel import (PackageModel, PackageConfigs,
-ConfigExecutor, ZoomExampleExecutor,ZoomExampleExecutorResponse,ZoomExampleExecutorOutputs,OutputImage)
+ConfigExecutor, ZoomExampleExecutor,ZoomExampleExecutorResponse,ZoomExampleExecutorOutputs,OutputImage,TextWriterExecutorOutputs,TextWriterExecutorResponse,TextWriterExecutor)
 
 
-def build_response(context):
+def build_response_zoom(context):
     outputImage = OutputImage(value=context.image)
     zoomExampleExecutorOutputs = ZoomExampleExecutorOutputs(outputImage=outputImage)
     zoomExampleExecutorResponse=ZoomExampleExecutorResponse(outputs=zoomExampleExecutorOutputs)
     zoomExampleExecutor=ZoomExampleExecutor(value=zoomExampleExecutorResponse)
     configExecutor=ConfigExecutor(value= zoomExampleExecutor)
+    packageConfigs = PackageConfigs(executor=configExecutor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+def build_response_textwriter(context):
+    outputImage = OutputImage(value=context.image)
+    textWriterExecutorOutputs = TextWriterExecutorOutputs(outputImage=outputImage)
+    textWriterExecutorResponse = TextWriterExecutorResponse(outputs=textWriterExecutorOutputs)
+    textWriterExecutor = TextWriterExecutor(value=textWriterExecutorResponse)
+    configExecutor = ConfigExecutor(value=textWriterExecutor)
     packageConfigs = PackageConfigs(executor=configExecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
